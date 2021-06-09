7ae55f4b72ae5cb63afe33e5bfdf0191643c44c4
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-09 16:39:18 -0500

    Added ArrayFloat program

---
+++ ArrayFloat.java
@@ -0,0 +1,12 @@
+public class ArrayFloat
+{
+public static void main(String[]args)
+{
+float[]numbers={5,10,15,20,25,30,35,40};
+Float[] f={5f,10f,15f,20f,25f,30f,35f,40f};
+for(Float f1:numbers)
+{
+System.out.println(f1);
+}
+}
+}
