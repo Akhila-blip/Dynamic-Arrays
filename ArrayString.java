16b0d8d2216e6086f5e99618a82d8e307ec7dccd
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-09 16:54:28 -0500

    Added ArrayString program

---
+++ ArrayString.java
@@ -0,0 +1,13 @@
+public class ArrayString
+{
+public static void main(String[]args)
+{
+String[]Names={"Akhila","Archana","aditya"};
+String[]str={"Archana","Akhila"};
+System.out.println(str);
+for(String str1:Names)
+{
+System.out.println(str1);
+}
+}
+}
