a8abfe91da18393180d219498f688422ce2cd1bf
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-09 16:03:59 -0500

    Added ArrayDynamicDouble proggram

---
+++ ArrayDynamicDouble.java
@@ -0,0 +1,13 @@
+public class ArrayDynamicDouble
+{
+public static void main(String[]args)
+{
+int[]numbers={1,2,3,4,5,6,7,8};
+Double[] d={1d,2d};
+System.out.println(d);
+for(Integer i:numbers)
+{
+System.out.println(i);
+}
+}
+}
