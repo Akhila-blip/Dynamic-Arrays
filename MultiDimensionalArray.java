39e5333ecbbca0a61b12b8b6e345a153097b642b
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-09 22:38:06 -0500

    Added MultiDimensionalArray program

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,10 @@
+public class MultiDimensionalArray
+{
+public static void main(String[]args)
+{
+int[][][]i={{{1,2,3},{4,5,6}}};
+int l=0;
+System.out.println(l);
+System.out.println("array"+i);
+}
+}
